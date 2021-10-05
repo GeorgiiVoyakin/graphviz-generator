b22a63379454efbd70e08d003b2b5501eb5682ec
Georgii Voyakin <voyaking@gmail.com>
2021-10-05 13:00:47 +0300

    Quick fix

--- graph.py
+++ graph.py
@@ -5,3 +5,15 @@ class Graph:
 
     def __str__(self) -> str:
         return 'Data: {}'.format(self.data.__str__())
+
+    def has_path(self, source: int, dest: int) -> bool:
+        '''
+        Returns True if there is path form source vertex to dest vertex 
+        '''
+        return self.data[source][dest] > 0 
+
+    def has_connection(self, vertex_a: int, vertex_b: int) -&gt; bool:
+        '''
+        Returns True if vertex_a has edge to vertex_b
+        '''
+        return self.data[vertex_a][vertex_b] > 0 or self.data[vertex_b][vertex_a] > 0
